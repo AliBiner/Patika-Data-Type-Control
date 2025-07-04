e05588d5d1bff14a76c25138d9b7a2947c88293a
Ali Biner <alibiner.github@gmail.com>
2025-07-04 17:13:13 +0300

    reverse String input to BigDecimal input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,52 +1,44 @@
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int inputSize = scanner.nextInt();
-        String[] values  = new String[inputSize];
-        for (int i = 0; i < inputSize; i++) {
-            values[i] = scanner.next();
+        BigDecimal[] values  = new BigDecimal[inputSize];
+        for (int i = 0; i <inputSize; i++) {
+            values[i] = scanner.nextBigDecimal();
         }
-        checkValue(values,0);
+        checkValue(values);
     }
 
     // This method checks if each value can fit in byte, short, int or long
-    public static void checkValue(String[] values,int index){
-        try {
-            // If all values are checked, stop the method
-            if (index >= values.length)
-                return;
-            else{
-                // Try to convert the value to a long number
-                long convertedValue = Long.parseLong(values[index]);
-
+    public static void checkValue(BigDecimal[] values){
+        for (int i = 0; i < values.length; i++) {
+            int isOverFlowMaxLong = values[i].compareTo(BigDecimal.valueOf(Long.MAX_VALUE));
+            int isOverFlowMinLong = values[i].compareTo(BigDecimal.valueOf(Long.MIN_VALUE));
+            if (isOverFlowMaxLong == 1 || isOverFlowMinLong == -1 ){
+                System.out.println(values[i] + " can't be fitted anywhere.");
+                continue;
+            }
+            else {
                 // Print that the number can be fitted
-                System.out.println(values[index] + " can be fitted in:");
+                System.out.println(values[i] + " can be fitted in:");
 
                 // Check if it fits in byte
-                if (convertedValue>=Byte.MIN_VALUE && convertedValue<=Byte.MAX_VALUE)
+                if(values[i].longValue()>=Byte.MIN_VALUE&&values[i].longValue()<=Byte.MAX_VALUE)
                     System.out.println("* byte");
 
                 // Check if it fits in short
-                if (convertedValue>=Short.MIN_VALUE && convertedValue<=Short.MAX_VALUE)
+                if (values[i].longValue()>=Short.MIN_VALUE && values[i].longValue()<=Short.MAX_VALUE)
                     System.out.println("* short");
 
                 // Check if it fits in int
-                if (convertedValue>=Integer.MIN_VALUE && convertedValue<=Integer.MAX_VALUE)
+                if (values[i].longValue()>=Integer.MIN_VALUE && values[i].longValue()<=Integer.MAX_VALUE)
                     System.out.println("* int");
-
                 // All long numbers can be stored in long
                 System.out.println("* long");
-
-                // Check the next value
-                checkValue(values,++index);
             }
-        }catch (NumberFormatException e){
-            // If the value is too big or not a number, show this message
-            System.out.println(values[index] + " can't be fitted anywhere.");
-            // Check the next value
-            checkValue(values,++index);
         }
     }
 }
